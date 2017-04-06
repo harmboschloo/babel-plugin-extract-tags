043fdf4014ca36d822f29829f0c41aac848fdf09
Harm Boschloo <harm@boschloo.net>
2017-04-06 23:03:30 +0200

    fixed name generation for code 0

--- src/utils/generateName.js
+++ src/utils/generateName.js
@@ -8,7 +8,7 @@ export const createGenerator = characters => {
       ? generateName(Math.floor(code / n), name + chars[code % n])
       : name;
 
-    return code => generateName(code, '');
+    return code => code > 0 ? generateName(code, '') : chars[0];
 }
 
 export const generateAlphabeticName =
