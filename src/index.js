bed01bd2f666efd77c0d2d44046a5f46a0b98c18
Harm Boschloo <harm@boschloo.net>
2017-04-10 20:28:16 +0200

    comment

--- src/index.js
+++ src/index.js
@@ -168,6 +168,7 @@ export const createPlugin = (createOptions = {}) =>
               outputFilePath
             };
 
+            // TODO test
             const outputProps = options.taggedCallback
               ? options.taggedCallback(taggedProps)
               : taggedProps;
